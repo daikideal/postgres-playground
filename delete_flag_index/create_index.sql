b10698148d2af0804be7c8b34b68e79014d03361
daikideal <54879050+daikideal@users.noreply.github.com>
2023-12-06 15:13:57 +0900

    Change: 検証に使用するカラムをemailではなくnameに変更

--- delete_flag_index/create_index.sql
+++ delete_flag_index/create_index.sql
@@ -1,8 +1,11 @@
 -- deleted に単体インデックス
 CREATE INDEX IF NOT EXISTS users_deleted_index ON users (deleted);
 
--- email と deleted の複合インデックス
-CREATE INDEX IF NOT EXISTS users_email_deleted_index ON users (email, deleted);
+-- name に単体インデックス
+CREATE INDEX IF NOT EXISTS users_name_index ON users (name);
 
--- deleted と email の順序を入れ替えた複合インデックス
-CREATE INDEX IF NOT EXISTS users_deleted_email_index ON users (deleted, email);
+-- name と deleted の複合インデックス
+CREATE INDEX IF NOT EXISTS users_name_deleted_index ON users (name, deleted);
+
+-- deleted と name の順序を入れ替えた複合インデックス
+CREATE INDEX IF NOT EXISTS users_deleted_name_index ON users (deleted, name);
